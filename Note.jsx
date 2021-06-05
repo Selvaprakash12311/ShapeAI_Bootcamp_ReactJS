f755944e333cf29a3347d088a0cd99855b15b429
Selvaprakash12311 <85387841+Selvaprakash12311@users.noreply.github.com>
2021-06-05 13:27:15 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Lamborghini</h1>
+      <p>
+        This car is an Italian brand,founded in 1963 by Ferruccio Lamborghini.
+        Lamborghini can go upto the speed of 350kph and can go from 0-100kph in
+        just 2.9 seconds. I love this car very much.
+      </p>
+    </div>
+  );
+}
+export default Note;
